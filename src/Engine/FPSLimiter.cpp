e5de09579889e4f450018ddbde9eb7a768bbcd8b
William Horvath <william@horvath.blog>
2025-11-11 00:57:45 -0800

    fix runaway fps limit (when using worse limiter option)

--- src/Engine/FPSLimiter.cpp
+++ src/Engine/FPSLimiter.cpp
@@ -17,18 +17,15 @@ void limit_frames(int target_fps) {
         const u64 frame_time_ns = Timing::NS_PER_SECOND / static_cast<u64>(target_fps);
         const u64 now = Timing::getTicksNS();
 
-        const bool use_1ms_mode = cv::fps_limiter_nobusywait.getBool();
-        const bool fps_max_yield = cv::fps_max_yield.getBool();
-
         // if we're ahead of schedule, sleep until next frame
         if(next_frame_time > now) {
-            if(use_1ms_mode) {  // sleep for 1ms every so often to reach the frametime target
+            if(cv::fps_limiter_nobusywait.getBool()) {  // sleep for 1ms every so often to reach the frametime target
                 const u64 sleep_time_ns = next_frame_time - now;
                 const u64 sleep_time_ms = sleep_time_ns / Timing::NS_PER_MS;
 
                 if(sleep_time_ms > 0) {
                     Timing::sleepMS(sleep_time_ms);
-                } else if(fps_max_yield) {
+                } else if(cv::fps_max_yield.getBool()) {
                     Timing::sleep(0);
                 }
             } else {  // precise sleeps per-frame
@@ -36,19 +33,12 @@ void limit_frames(int target_fps) {
                 const u64 sleep_time = std::min(next_frame_time - now, frame_time_ns);
                 Timing::sleepNSPrecise(sleep_time);
             }
+        } else if(cv::fps_max_yield.getBool()) {
+            Timing::sleep(0);
+            next_frame_time = Timing::getTicksNS();  // update "now" to reflect the time spent in yield
         } else {
-            // behind schedule or exactly on time
-            if(!use_1ms_mode) {
-                // in precise mode, reset to now to avoid accumulating error
-                if(fps_max_yield) {
-                    Timing::sleep(0);
-                    next_frame_time = Timing::getTicksNS();
-                } else {
-                    next_frame_time = now;
-                }
-            } else if(fps_max_yield) {  // in 1ms mode, don't reset, accumulate error is intentional
-                Timing::sleep(0);
-            }
+            // behind schedule or exactly on time, reset to now
+            next_frame_time = now;
         }
         // set time for next frame
         next_frame_time += frame_time_ns;
