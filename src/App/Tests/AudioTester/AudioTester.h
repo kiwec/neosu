6eec6528996843816d308a9acf66a04976fea3db
William Horvath <william@horvath.blog>
2026-01-30 17:53:31 -0800

    add an audio test app

---
+++ src/App/Tests/AudioTester/AudioTester.h
@@ -0,0 +1,79 @@
+// Copyright (c) 2026, WH, All rights reserved.
+#pragma once
+
+#ifndef AUDIOTESTER_H
+#define AUDIOTESTER_H
+
+#include "config.h"
+
+#include "App.h"
+#include "MouseListener.h"
+
+#if (defined(MCENGINE_FEATURE_SOLOUD) && defined(MCENGINE_FEATURE_BASS))
+
+#include <string>
+#include <vector>
+
+namespace Mc::Tests {
+
+class AudioTesterImpl;
+
+class AudioTester : public App, public MouseListener {
+    NOCOPY_NOMOVE(AudioTester)
+   public:
+    AudioTester();
+    ~AudioTester() override;
+
+    void draw() override;
+    void update() override;
+
+    void onResolutionChanged(vec2 newResolution) override;
+    void onDPIChanged() override;
+
+    void onFocusGained() override;
+    void onFocusLost() override;
+
+    void onMinimized() override;
+    void onRestored() override;
+
+    [[nodiscard]] bool isInGameplay() const override;
+    [[nodiscard]] bool isInUnpausedGameplay() const override;
+
+    void stealFocus() override;
+
+    bool onShutdown() override;
+
+    [[nodiscard]] Sound *getSound(ActionSound action) const override;
+
+    void showNotification(const NotificationInfo &notif) override;
+
+   public:
+    // keyboard
+    void onKeyDown(KeyboardEvent &e) override;
+    void onKeyUp(KeyboardEvent &e) override;
+    void onChar(KeyboardEvent &e) override;
+
+    // mouse
+    void onButtonChange(ButtonEvent event) override;
+    void onWheelVertical(int delta) override;
+    void onWheelHorizontal(int delta) override;
+
+   private:
+    friend AudioTesterImpl;
+    std::unique_ptr<AudioTesterImpl> m_impl;
+};
+}  // namespace Mc::Tests
+
+#else
+// nothing (need both bass and soloud to test for now)
+namespace Mc::Tests {
+
+class AudioTester : public App, public MouseListener {
+   public:
+    AudioTester() : App(), MouseListener() {}
+    ~AudioTester() override = default;
+};
+
+}  // namespace Mc::Tests
+#endif
+#endif
--- src/Engine/ConVars/ConVarDefs.h
+++ src/Engine/ConVars/ConVarDefs.h
@@ -133,6 +133,7 @@ CONVAR(snd_soloud_buffer, 0, CLIENT | NOSAVE, "SoLoud audio device buffer size (
 CONVAR(snd_soloud_backend, "MiniAudio"sv, CLIENT, R"(SoLoud backend, "MiniAudio" or "SDL3" (MiniAudio is default))");
 CONVAR(snd_soloud_offset_compensation_strategy, 1, CLIENT,
        R"(For debugging: 0 = naive (no auto offset), anything else = WSOLA pipeline model)");
+
 CONVAR(snd_sanity_simultaneous_limit, 128, CLIENT | NOSAVE,
        "The maximum number of overlayable sounds that are allowed to be active at once");
 CONVAR(snd_soloud_resampler, "linear", CLIENT,
